eae4907b50ad80451347d9b4501731f1dc25027d
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2025-01-27 20:08:49 +0530

    Create sum.cpp

---
+++ class/sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+   cout<<"Hello Sec 29"<<endl;
+   int n1,n2,sum=0;
+   cout<<"first number\n";
+   cin>>n1;
+   cout<<"second number\n";
+   cin>>n2;
+   sum=n1+n2;
+   cout<<n1<<"+"<<n2<<"="<<sum;
+   
+   
+   return 0;
+}

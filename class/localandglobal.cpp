ffae564add678fccc0e12dc1e2c0b6b142040c7b
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2025-01-27 20:06:01 +0530

    Create localandglobal.cpp

---
+++ class/localandglobal.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int y=10;
+int main(){
+   int x=20;
+   cout<<"locally declared variables"<<x<<endl;
+   cout<<"globally declared variables"<<y<<endl;
+   return 0;
+   }

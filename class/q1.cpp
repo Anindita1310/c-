e8d826f7be3a4e336850f16670d4348f3dd37609
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2025-01-27 20:05:15 +0530

    Create q1.cpp

---
+++ class/q1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+class Box{
+    private:
+    double length;
+    double width;
+    double height;
+    
+    public:
+     //setter method
+     void insertDetails(double l,double b,double h){
+       length=l;
+       width=b;
+       height=h;
+     }
+     double calculateVol(){
+         return length*width*height;
+     }
+};
+int main(){
+   Box box1;//object for class box
+   Box box2;
+   double volume=0.0;
+   double l,b,h;
+   
+  /* box1.height=5.0;
+   box1.length=12.0;
+   box1.width=4.0;*/
+   
+   /*//box2 details
+   box2.height=8.0;
+   box2.length=4.0;
+   box2.width=5.6;*/
+   cout<<"enter the length,width,height of a box"<<endl;
+   cin>>l>>b>>h;
+   
+   box1.insertDetails(l,b,h);
+   volume=box1.calculateVol();
+   cout<<"Volume is"<<volume<<endl;
+   
+   
+   return 0;
+   
+   
+}
